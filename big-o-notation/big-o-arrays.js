f035c5ddee772f552b39d76bd540cd6c7e2a91f7
Peter Martinez <peter.martinez@alianza.com>
2020-02-04 17:55:50 -0700

    Added arrays and objects as well as organized the big-o notation files

---
+++ big-o-notation/big-o-arrays.js
@@ -0,0 +1,23 @@
+/**
+Big O of Arrays
+----------------
+Insertion - It depends
+Removal - It depends
+Searching - O(n)
+Access - O(1)
+
+Big O of Array Methods
+-----------------------
+Array.push - O(1)
+Array.pop - O(1)
+Array.shift - O(n)
+Array.unshift - O(n)
+Array.concat - O(n)
+Array.slice - O(n)
+Array.splice - O(n)
+Array.sort - O(n * log n)
+Array.forEach/map/filter/reduce/etc - O(n)
+**/
+
+let names = ["Micheal", "Melissa", "Andrea"];
+let values = [true, {}, [], 2, "awesome"];
---
+++ big-o-notation/big-o-objects.js
@@ -0,0 +1,21 @@
+/**
+Big O of Objects
+----------------
+Insertion - O(1)
+Removal - O(1)
+Searching - O(n)
+Access - O(1)
+
+Big O of Object Methods
+-----------------------
+Object.Keys - O(n)
+Object.values - O(n)
+Object.entries - O(n)
+Object.hasOWnProperty - O(1)
+**/
+
+let instructor = {
+  firstName: "kelly",
+  isInstructor: true,
+  favoriteNumbers: [1, 2, 3, 4]
+};
